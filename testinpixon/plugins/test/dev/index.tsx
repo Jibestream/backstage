34fddd93c2eac940d5920d937902a52bd93729bd
swetalinasahoo01 <swetalina.sahoo@inpixon.com>
2021-06-22 16:56:53 -0400

    swagger implementation

---
+++ testinpixon/plugins/test/dev/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createDevApp } from '@backstage/dev-utils';
+import { testPlugin, TestPage } from '../src/plugin';
+
+createDevApp()
+  .registerPlugin(testPlugin)
+  .addPage({
+    element: <TestPage />,
+    title: 'Root Page',
+  })
+  .render();
